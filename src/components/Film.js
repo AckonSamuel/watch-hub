042d44225b7128f41fd6f64ae9592658f32f14b5
Ackon Samuel <ackons433@gmail.com>
2022-08-28 23:47:39 +0000

    Build film card component

--- src/components/Film.js
+++ src/components/Film.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import download from './download.jpg';
 
 const Film = () => (
-  <div>
-    <img src={download} alt="#" />
-    <div>
-      <p>
-        orem Ipsum is simply dummy
-        text of the printing and
-        typesetting industry. Lorem Ipsum has been the standard
-        dummy text ever since the 1500s,
-      </p>
-    </div>
-  </div>
+  <Card border="primary" style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={download} />
+    <Card.Body>
+      <Card.Title>Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text
+        to build on the card title
+        and make up the bulk of the card content.
+      </Card.Text>
+      <Button as="a" variant="primary">Go somewhere</Button>
+    </Card.Body>
+  </Card>
 );
 
 export default Film;

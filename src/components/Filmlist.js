95ff198970b7daa0c9d411506752ca655f4a917f
Ackon Samuel <ackons433@gmail.com>
2022-08-29 00:59:07 +0000

    Add props to Film component

--- src/components/Filmlist.js
+++ src/components/Filmlist.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import { FetchFilms } from '../redux/film';
 import Film from './Film';
 
-const Filmlist = () => (
-  <div />
-);
+const Filmlist = () => {
+  
+  const dispatch = useDispatch();
+  useEffect(() => dispatch(FetchFilms()), []);
+  const films = useSelector((state) => state.films, shallowEqual);
 
+  
+  return (
+    films.map((film) => {
+      <Film 
+      key={film.id}
+      title={film.title}
+      origin={film.original_title}
+      origin_romanised={film.original_title_romanised}
+      image={film.image}
+      />
+    })
+);
+}
 export default Filmlist;

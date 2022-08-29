ff9fb04810830dc69ff46ce5b68fe9ac12514cd1
Ackon Samuel <ackons433@gmail.com>
2022-08-29 09:47:55 +0000

    Update people's component file

--- src/components/details/People.js
+++ src/components/details/People.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Button from 'react-bootstrap/Button';
+import { Card } from 'react-bootstrap';
 
-const People = () => {
+const People = () => (
+    <Card>
+        <Card.Img />
+        <Card.Body>
+            <Card.Title>
+                Heloo man
+            </Card.Title>
+        </Card.Body>
+    </Card>
+);
 
-};
+export default People;

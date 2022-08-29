7f6614b293406a1ea64f3fa3bd2278e914ee478a
Ackon Samuel <ackons433@gmail.com>
2022-08-29 00:25:44 +0000

    Set up reducer, action creators and actions for fetch films

--- src/redux/film.js
+++ src/redux/film.js
@@ -1,1 +1,31 @@
 import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const FETCH_FILMS = 'watch-hub/film/films';
+// const POPULATE_FILMS = 'watch-hub/film/populateFilms';
+
+const initialState = [];
+
+const filmReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'watch-hub/film/films/fulfilled':
+            return [...action.payload];
+        default: 
+        return state;
+    }
+    
+}
+
+const FetchFilms = createAsyncThunk(
+    FETCH_FILMS,
+    async () => {
+        const res = await axios.get('https://ghibliapi.herokuapp.com/films');
+        return res;
+    },
+);
+
+export {
+    FetchFilms, 
+}
+
+export default filmReducer;

b273b93a95ad756fd5c957c0bd63ff873abcf069
Ackon Samuel <ackons433@gmail.com>
2022-08-29 00:41:00 +0000

    Add imports to Filmlist.js component file

--- src/components/Filmlist.js
+++ src/components/Filmlist.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
+import { FetchFilms } from '../redux/film';
+import Film from './Film';
 
 const Filmlist = () => (
   <div />

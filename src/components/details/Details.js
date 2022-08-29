04626069c47efb6364328261b245634fd4f3e6fe
Ackon Samuel <ackons433@gmail.com>
2022-08-29 09:51:59 +0000

    Add people to store

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
+import { FetchPeople } from '../../redux/people';
+import People from './People';
 
 const Details = () => (
   <div />

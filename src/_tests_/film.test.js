bfdd112bba317c2d25278c444d8f239e5b24ea7f
Ackon Samuel <ackons433@gmail.com>
2022-08-31 13:17:25 +0000

    Write test to check for default state in reducer

--- src/_tests_/film.test.js
+++ src/_tests_/film.test.js
@@ -2,3 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import '@testing-library/jest-dom';
 import filmReducer from '../redux/film';
+
+describe('test reducer', () => {
+    it('reducer has default state', () => {
+        const res = () => filmReducer(undefined, { type: undefined});
+        expect(res()).toEqual({
+            film: [],
+            filters: [],
+            search: '',
+          });
+    })
+});

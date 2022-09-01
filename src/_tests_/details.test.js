ea212779b425070599e62db113612e4892ac541c
Ackon Samuel <ackons433@gmail.com>
2022-09-01 17:00:50 +0000

    Add test for filmlist component

--- src/_tests_/details.test.js
+++ src/_tests_/details.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import store from '../redux/configureStore';
 import Details from '../components/details/Details';
